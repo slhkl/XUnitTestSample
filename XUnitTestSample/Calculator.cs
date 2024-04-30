a6cf4212aa9c3611adcfe20f3a532394c868f131
Salih Kol <salih.kol@detaysoft.com>
2024-04-30 12:17:50 +0300

    Add project files.

---
+++ XUnitTestSample/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+namespace XUnitTestSample
+{
+    public static class Calculator<T> where T : INumber<T>
+    {
+        public static T Sum(T number1, T number2)
+        {
+            return number1 + number2;
+        }
+
+        public static T Subtract(T number1, T number2)
+        {
+            return number1 - number2;
+        }
+
+        public static T Multiply(T number1, T number2)
+        {
+            return number1 * number2;
+        }
+
+        public static T Divide(T number1, T number2)
+        {
+            return number1 / number2;
+        }
+    }
+}
